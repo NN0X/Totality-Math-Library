48f996a1648970f43792158cec48af294f00c691
Tomasz Sarkowicz <nox@noxsoftware.pl>
2025-02-28 18:30:59 +0100

    added unoptimized slicing, reshaping and transposing for tensors

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,112 @@
 
 int main()
 {
+        // scalar tensor
+        Tensor<int, 0, 1> scalar;
+        std::cout << "Tensor scalar:\n";
+        scalar.print();
+
+        // vector tensor
         Tensor<float, 3, 27> tensor;
-        std::cout << "Tensor 3x3x3\n";
+        std::cout << "Tensor 3x3x3:\n";
         tensor.print();
-        Tensor<float, 2, 9> tensor2;
-        std::cout << "Tensor 3x3\n";
-        tensor2.print();
-        Tensor<float, 1, 3> tensor3;
-        std::cout << "Tensor vector 3\n";
-        tensor3.print();
+
+        // matrix tensor
+        Tensor<float, 2, 9> matrix;
+        std::cout << "Tensor 3x3:\n";
+        matrix.print();
+
+        // vector tensor assigment test
+        Tensor<float, 1, 3> vector;
+        std::cout << "Tensor vector 3:\n";
+        vector.print();
         tensor(0, 0, 0) = 1;
         tensor(0, 0, 1) = 2;
         tensor(0, 0, 2) = 3;
-        std::cout << "Tensor 3x3x3 after setting values\n";
+        std::cout << "Tensor 3x3x3 after setting values:\n";
         tensor.print();
+        Tensor<float, 2, 4> matrix1; // 2x2
+        Tensor<float, 2, 4> matrix2; // 2x2
+        matrix1(0, 0) = 1;
+        matrix1(0, 1) = 2;
+        matrix1(1, 0) = 3;
+        matrix1(1, 1) = 4;
+        std::cout << "Matrix 1 2x2:\n";
+        matrix1.print();
+        matrix2(0, 0) = 0;
+        matrix2(0, 1) = 5;
+        matrix2(1, 0) = 6;
+        matrix2(1, 1) = 7;
+        std::cout << "Matrix 2 2x2:\n";
+        matrix2.print();
+
+        //std::cout << "Tensor product of 2 2x2 matricies:\n";
+        //matrix1.product(matrix2).print();
+        //Tensor<float, 3, 27> tensorP(2.0f);
+        //std::cout << "Tensor product of 3x3x3 and 3x3x3:\n";
+        //tensor.product(tensorP).print();
+        //std::cout << "Tensor hadamard multiplication of 2 2x2 matricies:\n";
+
+        // hadamard multiplication test
+        matrix1.hadamard(matrix2).print();
+        std::cout << "Tensor addition of 2 2x2 matricies:\n";
+        (matrix1 + matrix2).print();
+        std::cout << "Tensor subtraction of 2 2x2 matricies:\n";
+        (matrix1 - matrix2).print();
+
+        std::cout << "Tensor hadamard multiplication of 3x3x3 and 3x3x3:\n";
+        tensor.hadamard(tensor).print();
+        std::cout << "Tensor addition of 3x3x3 and 3x3x3:\n";
+        (tensor + tensor).print();
+        std::cout << "Tensor subtraction of 3x3x3 and 3x3x3:\n";
+        (tensor - tensor).print();
+
+        // scalar operations test
+        std::cout << "Tensor scalar multiplication of 3x3x3:\n";
+        tensor *= 2.0f;
+        tensor.print();
+        std::cout << "Tensor scalar division of 3x3x3:\n";
+        tensor /= 4.0f;
+        tensor.print();
+
+        // slice test
+        Tensor<float, 5, 3125> tensorSlicable(5.0f);
+        Tensor<float, 2, 4> slice = tensorSlicable.slice<2, 4>(10, 18, 2);
+        std::cout << "Tensor slice of 5x5x5x5x5 from 10 to 18 with step 2 (2x2 matrix):\n";
+        slice.print();
+
+        // reshape test
+        const std::array<uint64_t, 10> shape = {10, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        Tensor<float, 10, 10> tensorReshapable(shape, 5.0f);
+        Tensor<float, 2, 10> reshaped = tensorReshapable.reshape<2>({2, 5});
+        std::cout << "Tensor reshaped from 10x1x1x1x1x1x1x1x1x1 to 2x5:\n";
+        reshaped.print();
+
+        // transpose test
+        Tensor<float, 2, 4> matrixTransposable;
+        matrixTransposable(0, 0) = 1;
+        matrixTransposable(0, 1) = 2;
+        matrixTransposable(1, 0) = 3;
+        matrixTransposable(1, 1) = 4;
+        std::cout << "Matrix 2x2:\n";
+        matrixTransposable.print();
+        Tensor<float, 2, 4> transposed = matrixTransposable.transpose(0, 1);
+        std::cout << "Matrix 2x2 transposed:\n";
+        transposed.print();
+
+        Tensor<float, 3, 27> tensorTransposable;
+        tensorTransposable(0, 0, 0) = 1;
+        tensorTransposable(0, 0, 1) = 2;
+        tensorTransposable(0, 0, 2) = 3;
+        tensorTransposable(0, 1, 0) = 4;
+        tensorTransposable(0, 1, 1) = 5;
+        tensorTransposable(0, 1, 2) = 6;
+        tensorTransposable(0, 2, 0) = 7;
+        tensorTransposable(0, 2, 1) = 8;
+        tensorTransposable(0, 2, 2) = 9;
+        std::cout << "Tensor 3x3x3:\n";
+        tensorTransposable.print();
+        Tensor<float, 3, 27> tensorTransposed = tensorTransposable.transpose(0, 2);
+        std::cout << "Tensor 3x3x3 transposed:\n";
+        tensorTransposed.print();
 }
--- src/tensor_cpu.hpp
+++ src/tensor_cpu.hpp
@@ -1,5 +1,5 @@
-#ifndef TENSOR_HPP
-#define TENSOR_HPP
+#ifndef TENSOR_CPU_HPP
+#define TENSOR_CPU_HPP
 
 #include <iostream>
 #include <cstdint>
@@ -11,6 +11,14 @@
 
 #include "heaparray.hpp"
 #include "assert_utils.hpp"
+
+// TODO: implement tensor operations:
+// - squeeze (remove dimensions with size 1)
+// - unsqueeze (add dimension with size 1)
+// - combine (combine two tensors along a dimension)
+// - product (generalized kronecker product)
+
+// TODO: optimize functions with multitheading and other
 
 template <typename T, uint64_t R, uint64_t S>
 class Tensor
@@ -89,6 +97,26 @@ public:
                 }
         }
 
+        Tensor(const std::array<uint64_t, R> &shape, const T &value) : mTensor(value)
+        {
+                if (shape.size() != R)
+                        throw std::runtime_error("Shape must have the same rank as the tensor.");
+                uint64_t shapeSize = 1;
+                for (uint64_t i = 0; i < R; i++)
+                        shapeSize *= shape[i];
+                if (shapeSize != S)
+                        throw std::runtime_error("Size inferred from shape must match the size of the tensor.");
+                mShape = shape;
+                mStrides.fill(1);
+                for (uint64_t i = 0; i < R; i++)
+                {
+                        for (uint64_t j = i + 1; j < R; j++)
+                        {
+                                mStrides[i] *= mShape[j];
+                        }
+                }
+        }
+
         ~Tensor() = default;
 
         Tensor<T, R, S> &operator=(const Tensor<T, R, S> &tensor)
@@ -112,6 +140,18 @@ public:
                 return mTensor[index];
         }
 
+        T &operator()(const std::array<uint64_t, R> &indices)
+        {
+                if (indices.size() != R)
+                        throw std::runtime_error("Number of arguments must match the rank of the tensor.");
+                uint64_t index = 0;
+                for (uint64_t i = 0; i < R; i++)
+                {
+                        index += mStrides[i] * indices[i];
+                }
+                return mTensor[index];
+        }
+
         template <typename... Args>
         const T &operator()(Args... args) const
         {
@@ -125,6 +165,179 @@ public:
                 return mTensor[index];
         }
 
+        const T &operator()(const std::array<uint64_t, R> &indices) const
+        {
+                if (indices.size() != R)
+                        throw std::runtime_error("Number of arguments must match the rank of the tensor.");
+                uint64_t index = 0;
+                for (uint64_t i = 0; i < R; i++)
+                {
+                        index += mStrides[i] * indices[i];
+                }
+                return mTensor[index];
+        }
+
+        T &operator[](const uint64_t index)
+        {
+                return mTensor[index];
+        }
+
+        const T &operator[](const uint64_t index) const
+        {
+                return mTensor[index];
+        }
+
+        T &at(const uint64_t index)
+        {
+                if (index >= S)
+                        throw std::out_of_range("Index out of range.");
+                return mTensor[index];
+        }
+
+        const T &at(const uint64_t index) const
+        {
+                if (index >= S)
+                        throw std::out_of_range("Index out of range.");
+                return mTensor[index];
+        }
+
+        Tensor<T, R, S> operator+(const Tensor<T, R, S> &tensor) const
+        {
+                Tensor<T, R, S> result = *this;
+                for (uint64_t i = 0; i < S; i++)
+                        result[i] += tensor[i];
+                return result;
+        }
+
+        Tensor<T, R, S> &operator+=(const Tensor<T, R, S> &tensor)
+        {
+                for (uint64_t i = 0; i < S; i++)
+                        mTensor[i] += tensor[i];
+                return *this;
+        }
+
+        Tensor<T, R, S> operator-(const Tensor<T, R, S> &tensor) const
+        {
+                Tensor<T, R, S> result = *this;
+                for (uint64_t i = 0; i < S; i++)
+                        result[i] -= tensor[i];
+                return result;
+        }
+
+        Tensor<T, R, S> &operator-=(const Tensor<T, R, S> &tensor)
+        {
+                for (uint64_t i = 0; i < S; i++)
+                        mTensor[i] -= tensor[i];
+                return *this;
+        }
+
+        Tensor<T, R, S> operator*(const T &scalar) const
+        {
+                Tensor<T, R, S> result = *this; 
+                for (uint64_t i = 0; i < S; i++)
+                        result[i] *= scalar;
+                return result;
+        }
+
+        Tensor<T, R, S> &operator*=(const T &scalar)
+        {
+                for (uint64_t i = 0; i < S; i++)
+                        mTensor[i] *= scalar;
+                return *this;
+        }
+
+        Tensor<T, R, S> operator/(const T &scalar) const
+        {
+                Tensor<T, R, S> result = *this; 
+                for (uint64_t i = 0; i < S; i++)
+                        result[i] /= scalar;
+                return result;
+        }
+
+        Tensor<T, R, S> &operator/=(const T &scalar)
+        {
+                for (uint64_t i = 0; i < S; i++)
+                        mTensor[i] /= scalar;
+                return *this;
+        }
+
+        template <uint64_t R2, uint64_t S2>
+        Tensor<T, R, S*S2> product(const Tensor<T, R2, S2> &tensor) const
+        {
+                // TODO: implement generalized kronecker product
+                return Tensor<T, R, S*S2>();
+        }
+
+        Tensor<T, R, S> hadamard(const Tensor<T, R, S> &tensor) const
+        {
+                Tensor<T, R, S> result;
+                for (uint64_t i = 0; i < S; i++)
+                        result[i] = mTensor[i] * tensor[i];
+                return result;
+        }
+
+        template <uint64_t R2, uint64_t S2>
+        Tensor<T, R2, S2> slice(uint64_t start, uint64_t end, uint64_t step) const
+        {
+                static_assert(R2 <= R, "Rank of the slice must be less than or equal to the rank of the tensor.");
+                static_assert(S2 <= S, "Size of the slice must be less than or equal to the size of the tensor.");
+
+                if (start >= S || end > S || start >= end)
+                        throw std::out_of_range("Invalid slice range.");
+                if (step == 0)
+                        throw std::invalid_argument("Step must be greater than zero.");
+                if ((end - start) % step != 0)
+                        throw std::invalid_argument("Slice range must be divisible by the step.");
+
+                if ((end - start) / step != S2)
+                        throw std::runtime_error("Size of the slice must match the size of the tensor.");
+
+                Tensor<T, R2, S2> result;
+                uint64_t sliceIndex = 0;
+                for (uint64_t i = start; i < end; i += step)
+                {
+                        result[sliceIndex++] = mTensor[i];
+                }
+                return result;
+        }
+
+        template <uint64_t R2>
+        Tensor<T, R2, S> reshape(const std::array<uint64_t, R2> &shape) const
+        {
+                if (shape.size() != R2)
+                        throw std::runtime_error("Shape must have the same rank as the tensor.");
+                Tensor<T, R2, S> result(shape);
+                for (uint64_t i = 0; i < S; i++)
+                {
+                        result[i] = mTensor[i];
+                }
+                return result;
+        }
+
+        Tensor<T, R, S> transpose(uint64_t dimA, uint64_t dimB) const
+        {
+                if (dimA >= R || dimB >= R)
+                        throw std::out_of_range("Invalid dimensions for transpose.");
+                if (dimA == dimB)
+                        return *this;
+                std::array<uint64_t, R> newShape = mShape;
+                std::swap(newShape[dimA], newShape[dimB]);
+                Tensor<T, R, S> result(newShape);
+                for (uint64_t i = 0; i < S; i++)
+                {
+                        std::array<uint64_t, R> indices;
+                        for (uint64_t j = 0; j < R; j++)
+                        {
+                                indices[j] = i / mStrides[j] % mShape[j];
+                        }
+                        std::array<uint64_t, R> newIndices = indices;
+                        std::swap(newIndices[dimA], newIndices[dimB]);
+                        result(newIndices) = (*this)(indices);
+                }
+
+                return result;
+        }
+
         void fill(const T &value)
         {
                 mTensor.fill(value);
@@ -135,16 +348,20 @@ public:
                 mTensor.zero();
         }
 
-        void print() const
+        void print(uint8_t precision) const
         {
                 static_assert(R <= 3, "Printing is only supported for tensors with rank <= 3.");
 
-                if constexpr (R == 1)
+                if constexpr (R == 0)
+                {
+                        std::cout << std::fixed << std::setprecision(precision) << mTensor[0] << "\n";
+                }
+                else if constexpr (R == 1)
                 {
                         std::cout << "| ";
                         for (uint64_t elementIndex = 0; elementIndex < S; elementIndex++)
                         {
-                                std::cout << std::fixed << std::setprecision(6) << mTensor[elementIndex] << " ";
+                                std::cout << std::fixed << std::setprecision(precision) << mTensor[elementIndex] << " ";
                         }
                         std::cout << "|\n";
                 }
@@ -155,7 +372,7 @@ public:
                                 std::cout << "| ";
                                 for (uint64_t colIndex = 0; colIndex < mShape[1]; colIndex++)
                                 {
-                                        std::cout << std::fixed << std::setprecision(6) << mTensor[rowIndex * mShape[1] + colIndex] << " ";
+                                        std::cout << std::fixed << std::setprecision(precision) << mTensor[rowIndex * mShape[1] + colIndex] << " ";
                                 }
                                 std::cout << "|\n";
                         }
@@ -170,13 +387,18 @@ public:
                                         for (uint64_t colIndex = 0; colIndex < mShape[2]; colIndex++)
                                         {
                                                 uint64_t tensorIndex = matrixIndex * mShape[1] * mShape[2] + rowIndex * mShape[2] + colIndex;
-                                                std::cout << std::fixed << std::setprecision(6) << mTensor[tensorIndex] << " ";
+                                                std::cout << std::fixed << std::setprecision(precision) << mTensor[tensorIndex] << " ";
                                         }
                                         std::cout << "| ";
                                 }
                                 std::cout << "\n";
                         }
                 }
+        }
+
+        void print() const
+        {
+                print(6);
         }
 
         uint64_t size() const
@@ -200,4 +422,4 @@ public:
         }
 };
 
-#endif // TENSOR_HPP
+#endif // TENSOR_CPU_HPP
