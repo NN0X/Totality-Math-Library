aadb16ba05d2a5ec389932da4b82f6a4b9119818
Tomasz Sarkowicz <nox@noxsoftware.pl>
2025-03-01 18:18:06 +0100

    setup for strassen alg implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,176 +1,33 @@
 #include <iostream>
+#include <cstdint>
+#include <array>
+#include <random>
+#include <chrono>
 
 #include "tensor_cpu.hpp"
 #include "matrix_cpu.hpp"
 #include "vector_cpu.hpp"
+#include "heaparray.hpp"
+
+template <typename T, uint64_t N>
+HeapArray<T, N> fillRandom()
+{
+        HeapArray<T, N> array;
+        for (uint64_t i = 0; i < N; i++)
+        {
+                array[i] = static_cast<T>(rand()) / static_cast<T>(RAND_MAX);
+        }
+        return array;
+}
 
 int main()
 {
-        // TENSOR
-
-        // scalar tensor
-        Tensor<int, 0, 1> scalar;
-        std::cout << "Tensor scalar:\n";
-        scalar.print();
-
-        // vector tensor
-        Tensor<float, 3, 27> tensor;
-        std::cout << "Tensor 3x3x3:\n";
-        tensor.print();
-
-        // matrix tensor
-        Tensor<float, 2, 9> matrix;
-        std::cout << "Tensor 3x3:\n";
-        matrix.print();
-
-        // vector tensor assigment test
-        Tensor<float, 1, 3> vector;
-        std::cout << "Tensor vector 3:\n";
-        vector.print();
-        tensor(0, 0, 0) = 1;
-        tensor(0, 0, 1) = 2;
-        tensor(0, 0, 2) = 3;
-        std::cout << "Tensor 3x3x3 after setting values:\n";
-        tensor.print();
-        Tensor<float, 2, 4> matrix1; // 2x2
-        Tensor<float, 2, 4> matrix2; // 2x2
-        matrix1(0, 0) = 1;
-        matrix1(0, 1) = 2;
-        matrix1(1, 0) = 3;
-        matrix1(1, 1) = 4;
-        std::cout << "Matrix 1 2x2:\n";
-        matrix1.print();
-        matrix2(0, 0) = 0;
-        matrix2(0, 1) = 5;
-        matrix2(1, 0) = 6;
-        matrix2(1, 1) = 7;
-        std::cout << "Matrix 2 2x2:\n";
-        matrix2.print();
-
-        //std::cout << "Tensor product of 2 2x2 matricies:\n";
-        //matrix1.product(matrix2).print();
-        //Tensor<float, 3, 27> tensorP(2.0f);
-        //std::cout << "Tensor product of 3x3x3 and 3x3x3:\n";
-        //tensor.product(tensorP).print();
-        //std::cout << "Tensor hadamard multiplication of 2 2x2 matricies:\n";
-
-        // hadamard multiplication test
-        matrix1.hadamard(matrix2).print();
-        std::cout << "Tensor addition of 2 2x2 matricies:\n";
-        (matrix1 + matrix2).print();
-        std::cout << "Tensor subtraction of 2 2x2 matricies:\n";
-        (matrix1 - matrix2).print();
-
-        std::cout << "Tensor hadamard multiplication of 3x3x3 and 3x3x3:\n";
-        tensor.hadamard(tensor).print();
-        std::cout << "Tensor addition of 3x3x3 and 3x3x3:\n";
-        (tensor + tensor).print();
-        std::cout << "Tensor subtraction of 3x3x3 and 3x3x3:\n";
-        (tensor - tensor).print();
-
-        // scalar operations test
-        std::cout << "Tensor scalar multiplication of 3x3x3:\n";
-        tensor *= 2.0f;
-        tensor.print();
-        std::cout << "Tensor scalar division of 3x3x3:\n";
-        tensor /= 4.0f;
-        tensor.print();
-
-        // slice test
-        Tensor<float, 5, 3125> tensorSlicable(5.0f);
-        Tensor<float, 2, 4> slice = tensorSlicable.slice<2, 4>(10, 18, 2);
-        std::cout << "Tensor slice of 5x5x5x5x5 from 10 to 18 with step 2 (2x2 matrix):\n";
-        slice.print();
-
-        // reshape test
-        const std::array<uint64_t, 10> shape = {10, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-        Tensor<float, 10, 10> tensorReshapable(shape, 5.0f);
-        Tensor<float, 2, 10> reshaped = tensorReshapable.reshape<2>({2, 5});
-        std::cout << "Tensor reshaped from 10x1x1x1x1x1x1x1x1x1 to 2x5:\n";
-        reshaped.print();
-
-        // transpose test
-        Tensor<float, 2, 4> matrixTransposable;
-        matrixTransposable(0, 0) = 1;
-        matrixTransposable(0, 1) = 2;
-        matrixTransposable(1, 0) = 3;
-        matrixTransposable(1, 1) = 4;
-        std::cout << "Matrix 2x2:\n";
-        matrixTransposable.print();
-        Tensor<float, 2, 4> transposed = matrixTransposable.transpose(0, 1);
-        std::cout << "Matrix 2x2 transposed:\n";
-        transposed.print();
-
-        Tensor<float, 3, 27> tensorTransposable;
-        tensorTransposable(0, 0, 0) = 1;
-        tensorTransposable(0, 0, 1) = 2;
-        tensorTransposable(0, 0, 2) = 3;
-        tensorTransposable(0, 1, 0) = 4;
-        tensorTransposable(0, 1, 1) = 5;
-        tensorTransposable(0, 1, 2) = 6;
-        tensorTransposable(0, 2, 0) = 7;
-        tensorTransposable(0, 2, 1) = 8;
-        tensorTransposable(0, 2, 2) = 9;
-        std::cout << "Tensor 3x3x3:\n";
-        tensorTransposable.print();
-        Tensor<float, 3, 27> tensorTransposed = tensorTransposable.transpose(0, 2);
-        std::cout << "Tensor 3x3x3 transposed:\n";
-        tensorTransposed.print();
-
-        // squeeze test
-        Tensor<float, 3, 27> tensorSqueezable({1, 1, 27});
-        std::cout << "Tensor 1x1x27:\n";
-        tensorSqueezable.print();
-        Tensor<float, 2, 27> tensorSqueezed = tensorSqueezable.squeeze();
-        std::cout << "Tensor 1x1x27 squeezed to 1x27:\n";
-        tensorSqueezed.print();
-
-        // unsqueeze test
-        Tensor<float, 3, 27> tensorUnsqueezed = tensorSqueezed.unsqueeze(0);
-        std::cout << "Tensor 1x27 unsqueezed to 1x1x27:\n";
-        tensorUnsqueezed.print();
-
-        // combine test
-        Tensor<float, 2, 4> matrixCombining1(1.0f);
-        Tensor<float, 2, 4> matrixCombining2(2.0f);
-        std::cout << "Matrix 2x2:\n";
-        matrixCombining1.print();
-        Tensor<float, 2, 8> matrixCombined = matrixCombining1.combine(matrixCombining2, 0);
-        std::cout << "Matrix 2x2 combined with 2x2 along axis 0 (vertical):\n";
-        matrixCombined.print();
-
-        // MATRIX
-
-        // matrix multiplication test
-        Matrix<float, 3, 3> matrix1M(2.0f);
-        std::cout << "Matrix 3x3:\n";
-        matrix1M.print();
-        Matrix<float, 3, 3> matrix2M(3.0f);
-        std::cout << "Matrix 3x3:\n";
-        matrix2M.print();
-        std::cout << "Matrix product of 2 3x3 matricies:\n";
-        (matrix1M * matrix2M).print();
-
-        // matrix transpose test
-        Matrix<float, 3, 3> matrixTransposableM;
-        matrixTransposableM(0, 0) = 1;
-        matrixTransposableM(0, 1) = 2;
-        matrixTransposableM(0, 2) = 3;
-        matrixTransposableM(1, 0) = 4;
-        matrixTransposableM(1, 1) = 5;
-        matrixTransposableM(1, 2) = 6;
-        matrixTransposableM(2, 0) = 7;
-        matrixTransposableM(2, 1) = 8;
-        matrixTransposableM(2, 2) = 9;
-        std::cout << "Matrix 3x3:\n";
-        matrixTransposableM.print();
-        Matrix<float, 3, 3> transposedM = matrixTransposableM.transpose();
-        std::cout << "Matrix 3x3 transposed:\n";
-        transposedM.print();
-
-        // VECTOR
-
-        Vector<float, 3> vector1(1.0f);
-        std::cout << "Vector 3:\n";
-        vector1.print();
+        Matrix<float, 20000, 20000> matrix1(fillRandom<float, 20000*20000>());
+        Matrix<float, 20000, 20000> matrix2(fillRandom<float, 20000*20000>());
+        auto start = std::chrono::high_resolution_clock::now();
+        Matrix<float, 20000, 20000> matrix3 = matrix1 * matrix2;
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diff = end - start;
+        std::cout << matrix3(0, 0) << "\n";
+        std::cout << "Matrix multiplication: " << diff.count() << " s\n";
 }
--- src/matrix_cpu.hpp
+++ src/matrix_cpu.hpp
@@ -3,21 +3,23 @@
 
 #include "tensor_cpu.hpp"
 
-// TODO: implement matrix operations:
-// - matrix determinant
-// - matrix inverse
-
-// TODO: determin at which size to use Strassen algorithm for matrix multiplication
+// TODO: determine at which size to use Strassen algorithm for matrix multiplication
 // TODO: implement Strassen algorithm for matrix multiplication
 
 // TODO: optimize functions with multitheading and other
 
+
+#define BIG_MATRIX_SIZE 500000000
+
+template <typename T, uint64_t R, uint64_t C, bool = (R * C < BIG_MATRIX_SIZE)>
+class Matrix;
+
 template <typename T, uint64_t R, uint64_t C>
-class Matrix : public Tensor<T, 2, R * C>
+class Matrix<T, R, C, true> : public Tensor<T, 2, R * C>
 {
 public:
-        Matrix() : Tensor<T, 2, R * C>() {}
-        Matrix(const T &value) : Tensor<T, 2, R * C>(value) {}
+        Matrix() : Tensor<T, 2, R * C>({R, C}) {}
+        Matrix(const T &value) : Tensor<T, 2, R * C>({R, C}, value) {}
         Matrix(const HeapArray<T, R * C> &matrix) : Tensor<T, 2, R * C>(matrix) {}
 
         ~Matrix() = default;
@@ -42,7 +44,7 @@ public:
                 return result;
         }
 
-        Matrix<T, R, C> operator*=(const Matrix<T, R, C> &matrix)
+        Matrix<T, R, C>& operator*=(const Matrix<T, R, C> &matrix)
         {
                 *this = *this * matrix;
                 return *this;
@@ -56,6 +58,231 @@ public:
                         for (uint64_t j = 0; j < C; j++)
                         {
                                 result(j, i) = (*this)(i, j);
+                        }
+                }
+                return result;
+        }
+
+        T determinant() const
+        {
+                static_assert(R == C, "Determinant is only possible for square matrices.");
+                if constexpr (R == 1)
+                {
+                        return (*this)(0, 0);
+                }
+                else if constexpr (R == 2)
+                {
+                        return (*this)(0, 0) * (*this)(1, 1) - (*this)(0, 1) * (*this)(1, 0);
+                }
+                else
+                {
+                        T det = 0;
+                        for (uint64_t i = 0; i < C; i++)
+                        {
+                                Matrix<T, R - 1, C - 1> submatrix;
+                                for (uint64_t j = 1; j < R; j++)
+                                {
+                                        for (uint64_t k = 0; k < C; k++)
+                                        {
+                                                if (k < i)
+                                                {
+                                                        submatrix(j - 1, k) = (*this)(j, k);
+                                                }
+                                                else if (k > i)
+                                                {
+                                                        submatrix(j - 1, k - 1) = (*this)(j, k);
+                                                }
+                                        }
+                                }
+                                det += ((*this)(0, i) * submatrix.determinant() * ((i % 2 == 0) ? 1 : -1));
+                        }
+                        return det;
+                }
+        }
+
+        Matrix<T, R, C> inverse() const
+        {
+                static_assert(R == C, "Inverse is only possible for square matrices.");
+                Matrix<T, R, C> result;
+                T det = determinant();
+                if (det == 0)
+                {
+                        throw std::runtime_error("Matrix is singular.");
+                }
+                if constexpr (R == 1)
+                {
+                        result(0, 0) = 1 / (*this)(0, 0);
+                }
+                else if constexpr (R == 2)
+                {
+                        result(0, 0) = (*this)(1, 1) / det;
+                        result(0, 1) = -(*this)(0, 1) / det;
+                        result(1, 0) = -(*this)(1, 0) / det;
+                        result(1, 1) = (*this)(0, 0) / det;
+                }
+                else
+                {
+                        for (uint64_t i = 0; i < R; i++)
+                        {
+                                for (uint64_t j = 0; j < C; j++)
+                                {
+                                        Matrix<T, R - 1, C - 1> submatrix;
+                                        for (uint64_t k = 0; k < R; k++)
+                                        {
+                                                for (uint64_t l = 0; l < C; l++)
+                                                {
+                                                        if (k < i && l < j)
+                                                        {
+                                                                submatrix(k, l) = (*this)(k, l);
+                                                        }
+                                                        else if (k < i && l > j)
+                                                        {
+                                                                submatrix(k, l - 1) = (*this)(k, l);
+                                                        }
+                                                        else if (k > i && l < j)
+                                                        {
+                                                                submatrix(k - 1, l) = (*this)(k, l);
+                                                        }
+                                                        else if (k > i && l > j)
+                                                        {
+                                                                submatrix(k - 1, l - 1) = (*this)(k, l);
+                                                        }
+                                                }
+                                        }
+                                        result(j, i) = submatrix.determinant() * (((i + j) % 2 == 0) ? 1 : -1) / det;
+                                }
+                        }
+                }
+                return result;
+        }
+};
+
+template <typename T, uint64_t R, uint64_t C>
+class Matrix<T, R, C, false> : public Tensor<T, 2, R * C>
+{
+public:
+        Matrix() : Tensor<T, 2, R * C>({R, C}) {}
+        Matrix(const T &value) : Tensor<T, 2, R * C>({R, C}, value) {}
+        Matrix(const HeapArray<T, R * C> &matrix) : Tensor<T, 2, R * C>(matrix) {}
+
+        ~Matrix() = default;
+
+        template <uint64_t R2, uint64_t C2>
+        Matrix<T, R, C2> operator*(const Matrix<T, R2, C2> &matrix) const
+        {
+                static_assert(C == R2, "Matrix multiplication is only possible if the number of columns of the first matrix is equal to the number of rows of the second matrix.");
+                Matrix<T, R, C2> result;
+                // TODO: implement Strassen algorithm for matrix multiplication
+                return result;
+        }
+
+        Matrix<T, R, C>& operator*=(const Matrix<T, R, C> &matrix)
+        {
+                *this = *this * matrix;
+                return *this;
+        }
+
+        Matrix<T, C, R> transpose() const
+        {
+                Matrix<T, C, R> result;
+                for (uint64_t i = 0; i < R; i++)
+                {
+                        for (uint64_t j = 0; j < C; j++)
+                        {
+                                result(j, i) = (*this)(i, j);
+                        }
+                }
+                return result;
+        }
+
+        T determinant() const
+        {
+                static_assert(R == C, "Determinant is only possible for square matrices.");
+                if constexpr (R == 1)
+                {
+                        return (*this)(0, 0);
+                }
+                else if constexpr (R == 2)
+                {
+                        return (*this)(0, 0) * (*this)(1, 1) - (*this)(0, 1) * (*this)(1, 0);
+                }
+                else
+                {
+                        T det = 0;
+                        for (uint64_t i = 0; i < C; i++)
+                        {
+                                Matrix<T, R - 1, C - 1> submatrix;
+                                for (uint64_t j = 1; j < R; j++)
+                                {
+                                        for (uint64_t k = 0; k < C; k++)
+                                        {
+                                                if (k < i)
+                                                {
+                                                        submatrix(j - 1, k) = (*this)(j, k);
+                                                }
+                                                else if (k > i)
+                                                {
+                                                        submatrix(j - 1, k - 1) = (*this)(j, k);
+                                                }
+                                        }
+                                }
+                                det += ((*this)(0, i) * submatrix.determinant() * ((i % 2 == 0) ? 1 : -1));
+                        }
+                        return det;
+                }
+        }
+
+        Matrix<T, R, C> inverse() const
+        {
+                static_assert(R == C, "Inverse is only possible for square matrices.");
+                Matrix<T, R, C> result;
+                T det = determinant();
+                if (det == 0)
+                {
+                        throw std::runtime_error("Matrix is singular.");
+                }
+                if constexpr (R == 1)
+                {
+                        result(0, 0) = 1 / (*this)(0, 0);
+                }
+                else if constexpr (R == 2)
+                {
+                        result(0, 0) = (*this)(1, 1) / det;
+                        result(0, 1) = -(*this)(0, 1) / det;
+                        result(1, 0) = -(*this)(1, 0) / det;
+                        result(1, 1) = (*this)(0, 0) / det;
+                }
+                else
+                {
+                        for (uint64_t i = 0; i < R; i++)
+                        {
+                                for (uint64_t j = 0; j < C; j++)
+                                {
+                                        Matrix<T, R - 1, C - 1> submatrix;
+                                        for (uint64_t k = 0; k < R; k++)
+                                        {
+                                                for (uint64_t l = 0; l < C; l++)
+                                                {
+                                                        if (k < i && l < j)
+                                                        {
+                                                                submatrix(k, l) = (*this)(k, l);
+                                                        }
+                                                        else if (k < i && l > j)
+                                                        {
+                                                                submatrix(k, l - 1) = (*this)(k, l);
+                                                        }
+                                                        else if (k > i && l < j)
+                                                        {
+                                                                submatrix(k - 1, l) = (*this)(k, l);
+                                                        }
+                                                        else if (k > i && l > j)
+                                                        {
+                                                                submatrix(k - 1, l - 1) = (*this)(k, l);
+                                                        }
+                                                }
+                                        }
+                                        result(j, i) = submatrix.determinant() * (((i + j) % 2 == 0) ? 1 : -1) / det;
+                                }
                         }
                 }
                 return result;
